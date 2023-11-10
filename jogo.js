df702c8b4335410f49752ff5d5f474cee0a2ccc8
nicolazin <106328512+nicolazin@users.noreply.github.com>
2023-11-10 09:34:45 -0300

    final

--- jogo.js
+++ jogo.js
@@ -9,7 +9,7 @@ const JOGADOR_0 = "0"
 
 let jogoAcabou = false; 
 
-document.addEventListener("click", (event) => ) {
+document.addEventListener("click", (event) =>  {
     if (event.target.matches(".celula") && !jogoAcabou) {
         !jogar(event.target.id);
     }
